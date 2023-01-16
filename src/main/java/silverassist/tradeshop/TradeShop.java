c6f3d890e6bf2ee1c471f1c763969ee6b84a97b1
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-01-16 20:18:39 +0900

    フォルダ作成部追加

--- src/main/java/silverassist/tradeshop/TradeShop.java
+++ src/main/java/silverassist/tradeshop/TradeShop.java
@@ -3,6 +3,10 @@ package silverassist.tradeshop;
 import org.bukkit.plugin.java.JavaPlugin;
 import silverassist.tradeshop.system.Setup;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public final class TradeShop extends JavaPlugin {
 
     private static JavaPlugin plugin = null;
@@ -11,7 +15,13 @@ public final class TradeShop extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin =this;
-        // Plugin startup logic
+        this.saveDefaultConfig();
+        try {
+            Files.createDirectories(Paths.get(this.getDataFolder()+"/data"));
+        } catch (IOException e) {
+            System.err.println("[TradeShop]Dataフォルダの作成に失敗しました");
+            getServer().getPluginManager().disablePlugin(this);
+        }
         SHOP_SYSTEM = new Setup(this);
         new Command(this,SHOP_SYSTEM);
 
