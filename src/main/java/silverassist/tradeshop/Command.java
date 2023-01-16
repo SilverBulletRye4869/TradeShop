53465afd5eb01392701fa5aa81d8fbab6f2616eb
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-01-16 20:20:04 +0900

    reloadコマンド、tab補完実装

--- src/main/java/silverassist/tradeshop/Command.java
+++ src/main/java/silverassist/tradeshop/Command.java
@@ -2,22 +2,26 @@ package silverassist.tradeshop;
 
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import silverassist.tradeshop.adminmenu.ShopEdit;
 import silverassist.tradeshop.adminmenu.ShopList;
 import silverassist.tradeshop.system.Setup;
 
+import java.util.List;
+
 import static silverassist.tradeshop.Util.sendPrefixMessage;
 
 public class Command implements CommandExecutor {
-    private final JavaPlugin plugin;
     private final Setup SHOP_SYSTEM;
 
     public Command(JavaPlugin plugin, Setup system){
-        this.plugin = plugin;
         this.SHOP_SYSTEM = system;
-        plugin.getCommand("tradeshop").setExecutor(this);
+        PluginCommand command = plugin.getCommand("tradeshop");
+        command.setExecutor(this);
+        command.setTabCompleter(new Tab());
     }
     @Override
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
@@ -29,7 +33,7 @@ public class Command implements CommandExecutor {
             return true;
         }
         String id = null;
-        if(args.length>2)id = args[1];
+        if(args.length>1)id = args[1];
 
         switch (args[0]){
             case "open":
@@ -39,21 +43,59 @@ public class Command implements CommandExecutor {
 
             case "create":
                 if(id==null)return true;
-                if(!CustomConfig.existYml(id)){
-                    CustomConfig.createYmlByID(id);
-                }
+                if(!CustomConfig.existYml(id)) CustomConfig.createYmlByID(id);
             case "edit":
                 if(id==null)return true;
                 if(!CustomConfig.existYml(id)){
                     sendPrefixMessage(p,"§c§lそのidのSHOPは存在しません");
                     return true;
                 }
-                new ShopEdit(p,id);
+                new ShopEdit(p,id).open();
                 return true;
             case "list":
-                new ShopList(p);
+                new ShopList(p).open(0);
+                return true;
+
+            case "reload":
+                if(id==null){
+                    //configのreload
+                    return true;
+                }
+                boolean result = SHOP_SYSTEM.reloadShop(id);
+                if(result)sendPrefixMessage(p,"§a§lSHOPをrelaodしました");
+                else sendPrefixMessage(p,"§c§lSHOPが存在しません");
+                return true;
+            case "reloadall":
+                SHOP_SYSTEM.getLoadedShopSet().forEach(SHOP_SYSTEM::reloadShop);
+                sendPrefixMessage(p,"§a全てのshopをreloadしました");
+                return true;
+            case "delete":
+                if(id==null)return true;
+                CustomConfig.deleteYmlByID(id);
+                SHOP_SYSTEM.deleteShop(id);
+                sendPrefixMessage(p,"§c§lSHOP『"+id+"』を正常に削除しました。");
+
                 return true;
         }
         return false;
+    }
+
+    private class Tab implements TabCompleter{
+
+        @Override
+        public List<String> onTabComplete(CommandSender sender, org.bukkit.command.Command command, String alias, String[] args) {
+            switch (args.length){
+                case 1:
+                    return List.of("open","create","edit","list","reload","reloadall","delete");
+                case 2:
+                    switch (args[0]){
+                        case "open":
+                        case "edit":
+                        case "reload":
+                            return ShopList.getShopList(args[1]);
+                    }
+            }
+            return null;
+        }
     }
 }
