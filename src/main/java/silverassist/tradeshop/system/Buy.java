bc572e109260871df3413193274d1e0ae2ec1886
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-01-17 21:17:31 +0900

    Log機能追加 & SHOP閉店機能追加

--- src/main/java/silverassist/tradeshop/system/Buy.java
+++ src/main/java/silverassist/tradeshop/system/Buy.java
@@ -6,7 +6,10 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 import silverassist.tradeshop.CustomConfig;
+import silverassist.tradeshop.TradeShop;
+
 
 import static silverassist.tradeshop.Util.*;
 
@@ -14,30 +17,32 @@ public class Buy {
     private static final int[] ITEM_PLACE = {10,11,12,19,20,21,28,29,30,24};
     private static final int[] LIME_PLACE = {14,15,16,23,25,32,33,34};
 
+    private final JavaPlugin plugin = TradeShop.getInstance();
     private final ItemStack[] ITEM = new ItemStack[10];
     private final int[] EXP_LV = new int[2]; //{必要経験値レベル,消費経験値レベル}
     private final Setup SHOP_SYSTEM;
     private final String SHOP_NAME;
+    private final YamlConfiguration YML;
 
     public Buy(Setup system,String id){
         this.SHOP_SYSTEM = system;
         this.SHOP_NAME = id;
-        YamlConfiguration y = CustomConfig.getYmlByID(id);
-        for(int i = 0;i<10;i++) ITEM[i] = y.getItemStack("item."+i,new ItemStack(Material.AIR));
-        this.EXP_LV[0] = y.getInt("exp.need",0);
-        this.EXP_LV[1] = y.getInt("exp.use",0);
+        YML = CustomConfig.getYmlByID(id);
+        for(int i = 0;i<10;i++) ITEM[i] = YML.getItemStack("item."+i,new ItemStack(Material.AIR));
+        this.EXP_LV[0] = YML.getInt("exp.need",0);
+        this.EXP_LV[1] = YML.getInt("exp.use",0);
     }
 
     public boolean open(Player p){
-        if(ITEM[9].getType().equals(Material.AIR))return false;
+        sendPrefixMessage(p,!YML.getBoolean("isOpen",true)+"-"+!plugin.getConfig().getBoolean("shop-enable",true));
         Inventory inv = Bukkit.createInventory(p,54,PREFIX+"§c§l"+this.SHOP_NAME);
 
         invFill(inv);
-        for(int i = 0;i < this.LIME_PLACE.length;i++)inv.setItem(this.LIME_PLACE[i],createItem(Material.LIME_STAINED_GLASS_PANE,"§r"));
+        for (int j : LIME_PLACE) inv.setItem(j, createItem(Material.LIME_STAINED_GLASS_PANE, "§r"));
 
-        for(int i = 0;i < this.ITEM_PLACE.length/* = 10 */;i++)inv.setItem(this.ITEM_PLACE[i], this.ITEM[i]);
-        if(this.EXP_LV[0]>0)inv.setItem(37,createItem(Material.EXPERIENCE_BOTTLE,"§a§l必要経験値: "+this.EXP_LV[0]));
-        if(this.EXP_LV[0]>0)inv.setItem(38,createItem(Material.EXPERIENCE_BOTTLE,"§e§l消費経験値: "+this.EXP_LV[1]));
+        for(int i = 0;i < ITEM_PLACE.length/* = 10 */;i++)inv.setItem(ITEM_PLACE[i], this.ITEM[i]);
+        if(this.EXP_LV[0]>0)inv.setItem(37,createItem(Material.EXPERIENCE_BOTTLE,"§a§l必要経験値: "+this.EXP_LV[0]+"Lv"));
+        if(this.EXP_LV[1]>0)inv.setItem(38,createItem(Material.EXPERIENCE_BOTTLE,"§e§l消費経験値: "+this.EXP_LV[1]+"Lv"));
 
         p.openInventory(inv);
         return true;
@@ -54,6 +59,7 @@ public class Buy {
         }
         for(int i = 0;i<9;i++){
             ItemStack item = this.ITEM[i];
+            if(item.getType().equals(Material.AIR))continue;
             if(p.getInventory().containsAtLeast(item,item.getAmount()))p.getInventory().removeItem(item);
             else{
                 for(int j = 0;j<i;j++)p.getInventory().addItem(this.ITEM[j]);
@@ -64,6 +70,7 @@ public class Buy {
         p.setLevel(p.getLevel()- EXP_LV[1]);
         p.getInventory().addItem(this.ITEM[9]);
         sendPrefixMessage(p,"§a§lトレード成功！");
+        Log.write(SHOP_NAME,p,this.ITEM[9]);
         return true;
     }
 }
